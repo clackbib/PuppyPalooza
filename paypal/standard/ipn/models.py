bff688b160cb827462a59ebd3a4d3b6df036b91b
clackbib <hokanla@spsu.edu>
2013-12-01 16:03:56 -0500

    Safe Commit

--- paypal/standard/ipn/models.py
+++ paypal/standard/ipn/models.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from urllib.request import urlopen
+
+#from urllib.request import urlopen
+from urllib2 import  urlopen
 from paypal.standard.models import PayPalStandardBase
 from paypal.standard.ipn.signals import *
 
